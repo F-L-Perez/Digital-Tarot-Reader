23ee9bbae41c7659d8605fa11d49b758b8b08c88
F. L. Pérez <franchescalperez@proton.me>
2026-08-30 20:33:41 -0400

    relief

--- cardRandomizer.js
+++ cardRandomizer.js
@@ -1,4 +1,4 @@
-// see "loadCards.js" for json loading and handling available card pool toggling
+// see "loadCards.js" for the preceding code for json loading and handling available card pool toggling
 
 function randomNumber(max) {
     // future me: the troublesome randomizer code below isn't necessary until there's toggles that have the deck NOT start at 0
@@ -9,16 +9,15 @@ function randomNumber(max) {
   
   function randomMajorArcana() {
     const availableCards = cardPool.length;
-    const arcanaNumber = randomNumber(availableCards); // give a random number between 0 and the amount of cards in the cardPool (based on toggle)
-    const card = cardPool[arcanaNumber]; // the card in the json with the randomized number
+    const arcanaNumber = randomNumber(availableCards); // give a random number between 0 and the amount of cards in the cardPool (amount changes by toggle)
+    const card = cardPool[arcanaNumber]; // it is simpler to assign a short variable for the randomized card, instead of repeating it over and over omg
     
-    const isReverseBinary = Math.round(Math.random(0, 1)) == 1; // random boolean, always returns a flat 1 or 0
-    const cardOrientation = isReverseBinary ? "Reversed" : "Upright"; // ? means if true then "Reversed", otherwise : "Upright" string
-    const cardMeaning = isReverseBinary ? card.Reverse : card.Upright; // return the card's reverse meaning in the card's json if true, and vice-versa
+    const isReverseBinary = Math.round(Math.random(0, 1)) == 1; // shorter way for random boolean 
+    const cardOrientation = isReverseBinary ? "Reversed" : "Upright"; // it's simpler to use ? and : for opposing if statements
+    const cardMeaning = isReverseBinary ? card.Reverse : card.Upright;
 
-
-    // Note: Since the ID and card name is fetched from the tarot deck json, with order and names inspired by RWS...
-    // Future themes that heavily deviate from "witt" (RWS), may need their seperate json (and toggle). like oracle decks. 
+    // Note: The image organizing was made to be more flexible/scalable. Since the ID and card name is fetched from the tarot deck json, using mainstream RWS name, order, meaning
+    // Future themes that heavily deviate from "witt" (RWS), the simplest solution is to have a seperate json (and toggle). like oracle decks. 
 
     const currentTheme = "Witt";
     const Image = `img/${currentTheme}/${card.ID}.jpg`; 
@@ -27,7 +26,7 @@ function randomNumber(max) {
     document.getElementById("meaning").innerHTML = cardMeaning;
     document.getElementById("cardImage").setAttribute('src', Image); // set the image with #cardImage, to have the src of the image path set above
     document.getElementById("cardImage").setAttribute('alt', card.Name + ` card in "` + currentTheme + `" style.`); // accessibility. the alt text of the image (e.g. the fool card in "witt" style)
-    document.getElementById("cardImage").style.transform = isReverseBinary ? "rotate(180deg)" : "rotate(0deg)"; // if reverse is true, then animate turning the card upside down with css transform function. or upright animation
+    document.getElementById("cardImage").style.transform = isReverseBinary ? "rotate(180deg)" : "rotate(0deg)"; // display the card position and animation with css3. simple.
   
   }
 
